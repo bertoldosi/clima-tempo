b66b8a99152ba87b50f679ca40df81433f8f32ab
bertoldosi <matheus.bertoldosi@gmail.com>
2022-05-30 22:23:16 -0300

    fix: Removendo erros no console

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta
+            http-equiv="Content-Security-Policy"
+            content="upgrade-insecure-requests"
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
