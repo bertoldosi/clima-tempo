fbc65d84d5bd5826d5a17c2083d80e53b2c88073
bertoldosi <matheus.bertoldosi@gmail.com>
2022-05-30 23:18:26 -0300

    feat: Implementando tema para noite

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 
+let hour = new Date().getHours();
+const isNigth = hour >= 6;
+
 const common = {
   backgroundColorWhite: "white",
   backgroundColorSecundary: "#2E2B2B",
@@ -24,7 +27,7 @@ export const darkTheme = {
     "linear-gradient(180deg, #0E398B 0%, rgba(24, 114, 165, 0.672688) 99.53%, rgba(19, 103, 151, 0.855911) 103.83%, rgba(28, 121, 173, 0.572576) 103.84%, rgba(33, 131, 187, 0.401042) 103.85%, rgba(45, 156, 219, 0) 103.86%, rgba(43, 152, 214, 0.0605227) 103.86%) no-repeat;",
 };
 
-const theme = lightTheme;
+const theme = isNigth ? darkTheme : lightTheme;
 
 const Theme = ({ children }) => (
   <ThemeProvider theme={theme}>{children}</ThemeProvider>
