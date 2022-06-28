d7ee093ba4ffc9c6d013d818b5d0cc0f5d76a1c3
bertoldosi <matheus.bertoldosi@gmail.com>
2022-06-28 10:57:45 -0300

    refactor: Ajustando data

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -1,42 +1,13 @@
+import { format } from "date-fns";
+import pt from "date-fns/locale/pt-BR";
+
 export const formatDate = (timestemp) => {
-  const week = [
-    "Domingo",
-    "Segunda",
-    "Terça",
-    "Quarta",
-    "Quinta",
-    "Sexta",
-    "Sábado",
-  ];
-
-  const month = [
-    "Jan",
-    "Fev",
-    "Mar",
-    "Abr",
-    "Mai",
-    "Jun",
-    "Jul",
-    "Ago",
-    "Sem",
-    "Out",
-    "Nov",
-    "Dez",
-  ];
-
   const correctingTimestemp = String(timestemp) + "000";
-  const dateLocaleString = new Date(
-    parseInt(correctingTimestemp)
-  ).toLocaleString();
-  const dateISO = new Date(dateLocaleString);
-  const date = dateISO;
+  const dateLocaleString = new Date(parseInt(correctingTimestemp));
 
-  const dayWeek = week[date.getDay()];
-  const dayMonth = String(date.getDate()).padStart(2, "0");
-  const monthYear =
-    month[String(date.getMonth()).padStart(2, "0").replace("0", "")];
+  const date = format(dateLocaleString, "EEEE', 'dd' de 'LLLL' de   'yyyy", {
+    locale: pt,
+  });
 
-  const dateFormat = `${dayWeek}, ${dayMonth} de ${monthYear}`;
-
-  return dateFormat;
+  return date;
 };
