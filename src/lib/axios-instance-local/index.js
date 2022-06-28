5be366839b6c064173142b6d8bbca1860ed65e9d
bertoldosi <matheus.bertoldosi@gmail.com>
2022-06-27 22:47:46 -0300

    feat: Corrigindo erro de produção

--- src/lib/axios-instance-local/index.js
+++ src/lib/axios-instance-local/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const isDevelopment = process.env.NODE_ENV === "development";
-const BASE_URL = isDevelopment ? "http://localhost:3333" : process.env.BASE_URL;
+const BASE_URL = isDevelopment
+  ? "http://localhost:3333"
+  : process.env.API_SCREEN;
 
 const instances = axios.create({
   baseURL: BASE_URL,
