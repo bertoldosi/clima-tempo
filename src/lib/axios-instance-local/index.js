666560bf000cd6e13de3a511efe7ad3b32ee0aac
bertoldosi <matheus.bertoldosi@gmail.com>
2022-06-27 22:45:03 -0300

    feat: Testando com env

--- src/lib/axios-instance-local/index.js
+++ src/lib/axios-instance-local/index.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const isDevelopment = process.env.NODE_ENV === "development";
-const BASE_URL = isDevelopment
-  ? "http://localhost:3333"
-  : "https://screen-clima-tempo.herokuapp.com/";
+const BASE_URL = isDevelopment ? "http://localhost:3333" : process.env.BASE_URL;
 
 const instances = axios.create({
   baseURL: BASE_URL,
