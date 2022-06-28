89e2e8b2ee7f247566d71a94b19a522f9db76e28
bertoldosi <matheus.bertoldosi@gmail.com>
2022-06-27 22:22:31 -0300

    fix: Corrigindo erro de deploy

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
+  publicRuntimeConfig: {
+    BASE_URL: process.env.BASE_URL || "http://localhost:3333",
+  },
 };
--- src/lib/axios-instance-local/index.js
+++ src/lib/axios-instance-local/index.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+import getConfig from "next/config";
+
+const { publicRuntimeConfig = {} } = getConfig() || {};
+const BASE_URL = publicRuntimeConfig.BASE_URL;
 
 const instances = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: BASE_URL,
 });
 
 export default instances;
