c3c9e77afeecab2769a8536e746d74a4635b2773
bertoldosi <matheus.bertoldosi@gmail.com>
2022-06-28 10:03:18 -0300

    feat: Corrigindo erros nas configurações de env

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
+
 module.exports = {
   compiler: {
     styledComponents: true,
+  },
+  env: {
+    API_SCREEN: process.env.API_SCREEN,
   },
 };
--- src/lib/axios-instance-local/index.js
+++ src/lib/axios-instance-local/index.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = process.env.API_SCREEN || "http://localhost:3333";
+const BASE_URL_PROD = process.env.API_SCREEN;
+const BASE_URL_DEV = "http://localhost:3333";
+
+const BASE_URL = BASE_URL_PROD || BASE_URL_DEV;
 
 const instances = axios.create({
   baseURL: BASE_URL,
