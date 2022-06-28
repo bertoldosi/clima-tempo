a7c79bc9262851fdec7b2efd1b0573fabf5bcbf3
bertoldosi <matheus.bertoldosi@gmail.com>
2022-06-27 22:54:39 -0300

    feat: Corrigindo env de prod

--- src/lib/axios-instance-local/index.js
+++ src/lib/axios-instance-local/index.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const isDevelopment = process.env.NODE_ENV === "development";
-const BASE_URL = isDevelopment
-  ? "http://localhost:3333"
-  : process.env.API_SCREEN;
+const BASE_URL = process.env.API_SCREEN || "http://localhost:3333";
 
 const instances = axios.create({
   baseURL: BASE_URL,
