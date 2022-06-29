39593cc1d7e3fc9cd0a5bcaaace4cc061ec121c2
bertoldosi <matheus.bertoldosi@gmail.com>
2022-06-29 15:55:41 -0300

    refactor: Ajustando mocks

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -3,35 +3,29 @@ const express = require("express");
 const { apiWeather, apiGeo } = require("../utils/axios-instances-external");
 const router = express.Router();
 
-const mockToday = require("./mocks/today.json");
-const mockTomorrow = require("./mocks/tomorrow.json");
-const mockWeek = require("./mocks/week.json");
-const mockCity = require("./mocks/city.json");
-
 const APPID = process.env.REACT_APP_APPID;
 
 router.get("/today", async (req, res) => {
   const { lat, lon } = req.query;
 
   try {
-    // const response = await apiWeather.get("/onecall", {
-    //   params: {
-    //     lat,
-    //     lon,
-    //     appid: APPID,
-    //     lang: "pt_br",
-    //   },
-    // });
+    const response = await apiWeather.get("/onecall", {
+      params: {
+        lat,
+        lon,
+        appid: APPID,
+        lang: "pt_br",
+      },
+    });
 
-    // const data = {
-    //   daily: response.data.daily[0],
-    //   current: response.data.current,
-    // };
+    const data = {
+      daily: response.data.daily[0],
+      current: response.data.current,
+    };
 
-    // res.status(200).send(data);
-    res.status(200).send(mockToday);
+    res.status(200).send(data);
   } catch (error) {
-    res.status(error.response.status).send(error);
+    res.status(error.response.status).send(error.response.data);
   }
 });
 
@@ -48,10 +42,9 @@ router.get("/tomorrow", async (req, res) => {
       },
     });
 
-    // res.status(200).send(response.data.daily[1]);
-    res.status(200).send(mockTomorrow);
+    res.status(200).send(response.data.daily[1]);
   } catch (error) {
-    res.status(error.response.status).send(error);
+    res.status(error.response.status).send(error.response.data);
   }
 });
 
@@ -59,19 +52,18 @@ router.get("/week", async (req, res) => {
   const { lat, lon } = req.query;
 
   try {
-    // const response = await apiWeather.get("/onecall", {
-    //   params: {
-    //     lat,
-    //     lon,
-    //     appid: APPID,
-    //     lang: "pt_br",
-    //   },
-    // });
+    const response = await apiWeather.get("/onecall", {
+      params: {
+        lat,
+        lon,
+        appid: APPID,
+        lang: "pt_br",
+      },
+    });
 
-    // res.status(200).send(response.data.daily);
-    res.status(200).send(mockWeek);
+    res.status(200).send(response.data.daily);
   } catch (error) {
-    res.status(error.response.status).send(error);
+    res.status(error.response.status).send(error.response.data);
   }
 });
 
@@ -79,28 +71,27 @@ router.get("/city", async (req, res) => {
   const { lat, lon } = req.query;
 
   try {
-    // const response = await apiGeo.get("/reverse", {
-    //   params: {
-    //     lat,
-    //     lon,
-    //     appid: APPID,
-    //   },
-    // });
+    const response = await apiGeo.get("/reverse", {
+      params: {
+        lat,
+        lon,
+        appid: APPID,
+      },
+    });
 
-    // const city = {
-    //   nome: response.data[0].name,
-    //   latitude: response.data[0].lat,
-    //   longitude: response.data[0].lon,
-    //   uf: {
-    //     uf: response.data[0].state,
-    //     nome: response.data[0].state,
-    //   },
-    // };
+    const city = {
+      nome: response.data[0].name,
+      latitude: response.data[0].lat,
+      longitude: response.data[0].lon,
+      uf: {
+        uf: response.data[0].state,
+        nome: response.data[0].state,
+      },
+    };
 
-    // res.status(200).send(city);
-    res.status(200).send(mockCity);
+    res.status(200).send(city);
   } catch (error) {
-    res.status(error.response.status).send(error);
+    res.status(error.response.status).send(error.response.data);
   }
 });
 
