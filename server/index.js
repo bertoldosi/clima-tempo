f09925b0722241c5f6f8bbcf804b22e917e61e7a
bertoldosi <matheus.bertoldosi@gmail.com>
2022-07-13 13:52:37 -0300

    refactor: Criando customização da api next

--- server/index.js
+++ server/index.js
@@ -1,23 +1,26 @@
-require("dotenv").config();
-const bodyParser = require("body-parser");
 const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
+const next = require("next");
 
-const app = express();
-const port = 3001;
-const config = {
-  origin: "*",
-};
-const corsInstance = cors(config);
+const port = process.env.PORT || 3001;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(helmet({ contentSecurityPolicy: false }));
-app.use(corsInstance);
+app.prepare().then(() => {
+  const server = express();
 
-require("./routes/weather")(app);
+  if (dev) {
+    require("./routes/mocks/weather")(server);
+  } else {
+    require("./routes/weather")(server);
+  }
 
-app.listen(port, () => {
-  console.log("Server start!, port:", port);
+  server.get("*/*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
 });
