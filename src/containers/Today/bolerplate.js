114c140c03a93dfc780dbf3d3c0536eac085ca1b
bertoldosi <matheus.bertoldosi@gmail.com>
2022-08-06 15:15:01 -0300

    fix: Corrigindo tempo de carregando dos icons

--- src/containers/Today/bolerplate.js
+++ src/containers/Today/bolerplate.js
@@ -3,13 +3,17 @@ import { formatDate } from "../../helpers/formatDate";
 import { Weathers } from "../../icons/Weathers";
 
 export const bolerplate = (data) => {
+  const icon = data.current.weather[0].icon;
+  const iconSize = 4;
+  const iconUrl = `http://openweathermap.org/img/wn/${icon}@${iconSize}x.png`;
+
   const weather = {
     tempDay: kelvinToCelsius(data.daily.temp.day),
     tempNight: kelvinToCelsius(data.daily.temp.night),
     temp: kelvinToCelsius(data.current.temp),
     sensation: kelvinToCelsius(data.current.feels_like),
     textWeather: data.current.weather[0].description,
-    icon: Weathers(data.current.weather[0].icon, 4),
+    iconUrl,
     date: formatDate(data.current.dt),
   };
 
--- src/icons/Weathers.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Weathers = (icon, size) => {
-  return <img src={`http://openweathermap.org/img/wn/${icon}@${size}x.png`} />;
-};
