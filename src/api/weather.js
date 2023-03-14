cccfc62e346f1e49930f548bd55ca122ec94e5fc
bertoldosi <matheus.bertoldosi@gmail.com>
2023-03-14 10:12:07 -0300

    fix: Corrindo erro ao chamar BFF

--- src/api/weather.js
+++ src/api/weather.js
@@ -2,20 +2,20 @@ import apiGateway from "../lib/axios-instance-local";
 
 export const GetWeatherToday = async (lat, lon) => {
   const params = { lat, lon };
-  return apiGateway.get("weather/today", { params });
+  return apiGateway.get("/weather/today", { params });
 };
 
 export const GetWeatherTomorrow = async (lat, lon) => {
   const params = { lat, lon };
-  return apiGateway.get("weather/tomorrow", { params });
+  return apiGateway.get("/weather/tomorrow", { params });
 };
 
 export const GetWeatherWeek = async (lat, lon) => {
   const params = { lat, lon };
-  return apiGateway.get("weather/week", { params });
+  return apiGateway.get("/weather/week", { params });
 };
 
 export const GetWeatherCity = async (lat, lon) => {
   const params = { lat, lon };
-  return apiGateway.get("weather/city", { params });
+  return apiGateway.get("/weather/city", { params });
 };
